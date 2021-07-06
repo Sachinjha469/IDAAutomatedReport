44f96107a744162e01959803c6865d475b2529b1
akashkrshukla <akashshukla778@gmail.com>
2021-07-06 16:38:53 +0530

    School Wise Report Added

--- generatePDF.py
+++ generatePDF.py
@@ -9,78 +9,42 @@ subskill = ['Comprehension', 'Vocabulary', 'Logical Reasoning', 'Verbal Reasonin
 
 
 async def main():
-    info_df = pd.read_csv("Para Article Data - Page 2 (1).csv")
-    for index in range(0, info_df.shape[0]):
-        # await create_graph()
+    school_df = pd.read_csv("Schoolwise report.csv")
+    school_list = school_df.groupby(school_df.columns.values[0])
+    for school in school_list:  # .count()
+        student_df = pd.read_csv("Schoolwise report.csv")
+        student_list = student_df.loc[student_df['school name'] == school[0]]
+        print(student_list.shape[0])
 
+        await create_graph()
         # open the HTML page.
         browser = await launch()
         page = await browser.newPage()
         await page.goto('file:///Users/akash/Projects/python/IDAAutomatedReport/htmlfinal/index.html')
         await page.emulateMedia('screen')
-
-        # get the data
-        #  Set the data.
-        name = info_df.values[index][1]
-        school_name = info_df.values[index][2]
-        grade = info_df.values[index][3]
-        score_str = str(info_df.values[index][4]).split("/")
-        obtained_mark, total_marks = int(score_str[0]), int(score_str[1])
-        nat_rank = info_df.values[index][5]
-        avg_nat_marks = int(info_df.values[index][6])
-        #  conversion factor to the percentage
-        cnv_factor = 100 / total_marks
-        your_score = f"{obtained_mark}/{total_marks}"
-        avg_nat_score = f"{avg_nat_marks}/{total_marks}"
-        your_score_lbl = str((round(obtained_mark * cnv_factor, 2)))
-        avg_score_lbl = str(round(avg_nat_marks * cnv_factor, 2))
-        your_score_desc = f"<strong>Your Score:</strong> Total number of correct answers/Total number of questions attempted. Example: Your Score = {obtained_mark}/{total_marks} denotes you answered {obtained_mark} out of {total_marks} questions correctly where {total_marks} is the maximum number of questions in the challenge."
-        nat_score_desc = f"<strong>National Average Score:</strong> This is the Average number of correct answers for the respective grade category/Maximum number of questions. For Example: National Score = {avg_nat_marks}/{total_marks} denotes on an average students answered {avg_nat_marks} out of {total_marks} questions correctly."
-
         # set the data to PDF
-        await page.evaluate(f" document.getElementById('name').innerHTML = `{name}`")
-        await page.evaluate(f" document.getElementById('school_name').innerHTML = `{school_name}`")
-        await page.evaluate(f" document.getElementById('grade').innerHTML = `{grade}`")
-        await page.evaluate(f" document.getElementById('nat_rank').innerHTML = `{nat_rank}`")
-        await page.evaluate(f" document.getElementById('score').innerHTML = `{your_score}`")
-        await page.evaluate(f" document.getElementById('your_score').innerHTML = `{your_score}`")
-        await page.evaluate(f" document.getElementById('nat_avg_score').innerHTML = `{avg_nat_score}`")
-        await page.evaluate(f" document.getElementById('your_score_desc').innerHTML = `{your_score_desc}`")
-        await page.evaluate(f" document.getElementById('nat_score_desc').innerHTML = `{nat_score_desc}`")
+        await page.evaluate(f" document.getElementById('school_name_c').innerHTML = `{school[0]}`")
+        await page.evaluate(f" document.getElementById('school_name').innerHTML = `{school[0]}`")
+        await page.evaluate(f" document.getElementById('ptc_count').innerHTML = ` Number of Participants: {student_list.shape[0]}`")
 
-        # Plot the bar chart
-        score_string = """() =>  {  var chart = new CanvasJS.Chart('chartContainer', { title:{text: 'Score Report'}, axisY: {title: 'Percentage Accuracy', interval: 10}, data: [{dataPoints: [{x: 1, y: %s, label: 'Your Score', indexLabel: '%s'},{ x: 2, y: %s,  label: 'National Average Score', indexLabel: '%s' }]}]});chart.render();}""" % (
-            obtained_mark * cnv_factor, your_score_lbl, avg_nat_marks * cnv_factor, avg_score_lbl)
-        await page.evaluate(score_string)
-
-        # plot the skill wise table
-        skill_df = pd.read_csv("Performance across Skills & Sub-Skills - Sheet1.csv")
-        skill_node = ""
-        s_header_node = "<thead><tr><td>Skill</td><td>Description</td><td>Right Answer</td><td>Wrong Answer</td></tr></thead>"
-        skill_node += s_header_node
-        for row in range(0, (skill_df.shape[0])):
-            skill_node += f"<tr><td>{skill_df.values[row][0]}</td><td>{skill_df.values[row][1]}</td><td>{skill_df.values[row][2]}</td><td>{skill_df.values[row][3]}</td></tr>"
-        skill_string = f" document.getElementById('skill_wise').innerHTML = `{skill_node}`"
-        await page.evaluate(skill_string)
-
-        list_df = pd.read_csv("FEC Certificates - Sheet6.csv")
+        # plot the table
         child_node = ""
         index = 0
-        q_header_node = "<table><thead><tr><td>S.No</td><td>Skill Name</td><td>Subskill Name</td><td>Question Asked</td><td>Your Answer</td><td>Correct Answer</td><td>Result</td></tr></thead>"
-        q_header_node_with_margin = "<table style='margin-top: 50px !important;'><thead><tr><td>S.No</td><td>Skill Name</td><td>Subskill Name</td><td>Question Asked</td><td>Your Answer</td><td>Correct Answer</td><td>Result</td></tr></thead>"
-        for row in range(0, (list_df.shape[0])):
+        q_header_node = "<table><thead><tr><td>S.No</td><td>Name</td><td>Grade</td><td>Communication</td><td>Critical Thinking</td><td>Digital Literacy</td><td>General Knowledge</td><td>Technology Aptitude</td></tr></thead>"
+        q_header_node_with_margin = "<table style='margin-top: 50px !important;'><thead><tr><td>S.No</td><td>Name</td><td>Grade</td><td>Communication</td><td>Critical Thinking</td><td>Digital Literacy</td><td>General Knowledge</td><td>Technology Aptitude</td></tr></thead>"
+        for row in range(0, (student_list.shape[0])):
             if index % 10 == 0:
                 if index != 0:
                     child_node += "</table><p style='page-break-after:always;'/>"
                     child_node += q_header_node_with_margin
                 else:
                     child_node += q_header_node
-            child_node += f"<tr><td>{list_df.values[row][0]}</td><td>{list_df.values[row][1]}</td><td>{list_df.values[row][2]}</td><td>{list_df.values[row][3]}</td><td>{list_df.values[row][4]}</td><td>{list_df.values[row][5]}</td><td>{list_df.values[row][6]}</td></tr>"
+            child_node += f"<tr><td>{row + 1}</td><td>{student_list.values[row][1]}</td><td>{student_list.values[row][2]}</td><td>{student_list.values[row][3]}</td><td>{student_list.values[row][4]}</td><td>{student_list.values[row][5]}</td><td>{student_list.values[row][6]}</td><td>{student_list.values[row][7]}</td></tr>"
             index += 1
         child_node += "</table><p style='page-break-after:always;'/>"
-        insert_string = f" document.getElementById('question_wise').innerHTML = `{child_node}`"
+        insert_string = f" document.getElementById('school_wise').innerHTML = `{child_node}`"
         await page.evaluate(insert_string)
-        await page.pdf({'path': 'IDAReport.pdf', 'displayHeaderFooter': False,
+        await page.pdf({'path': "{}.pdf".format(school[0]), 'displayHeaderFooter': False,
                         'margin': {'top': '35', 'right': '10', 'bottom': '35', 'left': '10'},
                         '-webkit-print-color-adjust': True, 'printBackground': True})
         await browser.close()
@@ -88,3 +52,15 @@ async def main():
 
 
 asyncio.get_event_loop().run_until_complete(main())
+
+
+# school_df = pd.read_csv("Schoolwise report.csv")
+# school_list = school_df.groupby(school_df.columns.values[0])
+#
+# for school in school_list:  # .count()
+#     student_df = pd.read_csv("Schoolwise report.csv")
+#     school_name = student_df['school name'][0]
+#     # test = student_df.loc["school_name" != ""]
+#     test = student_df.loc[student_df['school name'] == school[0]]
+#     print(student_df['school name'] == school[0])
+#     # print(student_df['school name'][0])
